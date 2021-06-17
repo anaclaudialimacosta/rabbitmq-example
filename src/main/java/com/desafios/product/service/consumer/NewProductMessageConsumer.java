a213d5e322831435cf2588dba2e46c1c0b724397
anaclaudialimacosta <almeidannynha@gmail.com>
2021-06-17 00:30:37 -0300

    Add Consumer

--- src/main/java/com/desafios/product/service/consumer/NewProductMessageConsumer.java
+++ src/main/java/com/desafios/product/service/consumer/NewProductMessageConsumer.java
@@ -1,7 +1,15 @@
 package com.desafios.product.service.consumer;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NewProductMessageConsumer {
+
+    private  static final String QUEUE_NAME = "course.queue";
+
+    @RabbitListener(queues = QUEUE_NAME)
+    public void listen(String message){
+        System.out.println(message);
+    }
 }
